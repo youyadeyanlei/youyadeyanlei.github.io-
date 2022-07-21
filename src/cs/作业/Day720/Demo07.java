a99c14e9b21dc77d95b3d07868911f1b2b9e527f
youyadeyanlei <wt227139>
2022-07-21 17:13:44 +0800

    更新2022/7/21

--- src/cs/作业/Day720/Demo07.java
+++ src/cs/作业/Day720/Demo07.java
@@ -13,22 +13,30 @@ public class Demo07 {
     public static void main(String[] args) {
         List list = new ArrayList<>(80);
         //     System.out.println(list);
-        for (int i = 1; i < 81; i++) {
+        for (int i = 1; i < 10; i++) {
             list.add(i);
         }
-   //     System.out.println(list);
-
-        int n = 1;
-        int i = 1;
-        while (list.size() > 2) {
-            if (n == 3) {
-                list.remove(i);
-                n=0;
+        for (int i = 1; i <4 ; i++) {
+            if (i==3){
+                list.remove(i-1);
             }
+        }
+        int n=1;int i=0;
+        while (list.size()>2){
+
+            if (n==3){
+                list.remove(i-1);
+            }
+
             n++;
-            i++;
         }
         System.out.println(list);
+//        list.remove(2);
+   //     System.out.println(list);
+
+//        while (list.size() > 2) {
+//        }
+ //       System.out.println(list);
 
     }
 
---
+++ src/cs/作业/Day721/Demo01.java
@@ -0,0 +1,13 @@
+package cs.作业.Day721;
+
+public class Demo01 {
+
+    public static void main(String[] args) {
+    String[] strings=new String[10];
+    
+        for (int i = 0; i < strings.length; i++) {
+
+        }
+
+    }
+}
